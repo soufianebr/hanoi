7b67cbff930c6eafbe3febd8a0db6eed7ea1ea01
soufianebr <berkanesoufiane@hotmail.com>
2018-02-03 16:10:33 +0100

    Add files via upload

---
+++ DisqueTest.java
@@ -0,0 +1,28 @@
+package cfranc.hanoi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DisqueTest {
+
+	@Test
+	public void testCompareTo() {
+		Disque petit = new Disque(1);
+		Disque moyen = new Disque(2);
+		Disque grand = new Disque(3);
+		assertEquals("petit.compareTo(moyen)",-1, petit.compareTo(moyen));
+		assertEquals("moyen.compareTo(grand)",-1, moyen.compareTo(grand));
+		// TODO : à compléter
+		assertEquals("petit.compareTo(grand)",-1, petit.compareTo(grand));
+
+		assertEquals("grand.compareTo(moyen)",1, grand.compareTo(moyen));
+		assertEquals("moyen.compareTo(petit)",1, moyen.compareTo(petit));
+		assertEquals(moyen.compareTo(petit),1, grand.compareTo(petit));
+
+		assertEquals("petit.compareTo(petit)",0, petit.compareTo(petit));
+		assertEquals("moyen.compareTo(moyen)",0, moyen.compareTo(moyen));
+		assertEquals("grand.compareTo(grand)",0, grand.compareTo(grand));
+	}
+
+}
---
+++ TourTest.java
@@ -0,0 +1,60 @@
+package cfranc.hanoi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TourTest {
+
+	@Test
+	public void testDisqueDansTourVide() 
+    {
+        Tour t = new Tour(3);
+        Disque d = new Disque(1);
+        boolean b = t.empiler(d);
+        assertEquals(true, b);
+	}
+
+	
+    public void testDisquesDansTourVide()
+    {
+        Tour t = new Tour(3);
+        Disque d1 = new Disque(2);
+        Disque d2 = new Disque(1);
+        boolean b1 = t.empiler(d1);
+        boolean b2 = t.empiler(d2);
+        assertEquals(true, b1);
+        assertEquals(true, b2);
+    }
+    
+    public void testTourPleine()
+    {
+        Tour t = new Tour(1);
+        Disque d1 = new Disque(2);
+        Disque d2 = new Disque(1);
+        t.empiler(d1);
+        boolean b = t.empiler(d2);
+        assertEquals(false, b);
+    }
+    
+    public void testDiametreSuperieur()
+    {
+        Tour t = new Tour(3);
+        Disque d1 = new Disque(2);
+        Disque d2 = new Disque(3);
+        t.empiler(d1);
+        boolean b = t.empiler(d2);
+        assertEquals(false, b);
+    }
+    
+    public void testDiametreSuperieurTourPleine()
+    {
+        Tour t = new Tour(1);
+        Disque d1 = new Disque(2);
+        Disque d2 = new Disque(3);
+        t.empiler(d1);
+        boolean b = t.empiler(d2);
+        assertEquals(false, b);
+    }
+}
+
